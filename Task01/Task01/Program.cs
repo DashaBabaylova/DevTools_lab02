a347f40f37f9d340df27ebc82f973b8d62a9f695
DashaBabaylova <Dazed1203@mail.ru>
2023-02-25 16:34:29 +0400

    add task01 files

---
+++ Task01/Task01/Program.cs
@@ -0,0 +1,8 @@
+﻿PowerCollections.Stack<int> stack = new PowerCollections.Stack<int>();
+stack.Push(0);
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+
+Console.WriteLine(stack.Capacity);
+Console.WriteLine(stack.Pop());
